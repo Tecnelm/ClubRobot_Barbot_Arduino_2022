c87b8f90f746e989ac8d9519a22981d667985aa6
Clement <tecnelm91@gmail.com>
2022-02-28 22:52:04 +0100

    Ajout de la structure de cuve

---
+++ main/tank.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include "tank.hpp"
+#include "config.h"
+
+
+void tank_init(struct tank* tank)
+{
+  pinMode(tank->pump,OUTPUT)
+}
+
+void refill_tank(struct tank* tank)
+{
+  if (AnalogRead(tank->sensor) > THRESHOLD_TANK_FULL)
+  {
+    PRINT("START REFILL MAIN TANK")
+    pinMode(tank->pump,OPEN);
+    while(AnalogRead(tank->sensor) > THRESHOLD_TANK_FULL);
+    pinMode(tank-> pump,CLOSE);
+    PRINT("END REFILLING MAIN TANK")
+  }
+  
+}
+
+tank_t tank = {
+  .init = tank_init;
+  .refill_tank = refill_tank;
+  .pump = PIN_TANK_PUMP;
+  .sensor = PIN_TANK_SENSOR;
+};
---
+++ main/tank.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef struct tank {
+  void (*init)(struct tank* tank);
+  void (*refill_tank)(struct tank* tank);
+  const int pump;
+  const int sensor;
+
+}tank_t;
+
