37a7fade2045a7001cc5452413202e97f62dec33
Clement <tecnelm91@gmail.com>
2022-03-07 20:29:41 +0100

    Modification majeur
    Modification des fonctions afin  de régler les problème de syntaxe 
    changement de la logique pour les déplacements 
    ajout de la fonction move qui permet d'effectuer le déplacement

--- main/chariot.cpp
+++ main/chariot.cpp
@@ -5,109 +5,138 @@
 Servo servo_chariot;
 
 
-void setup() {
-
-}
-
-
-
 void init_chariot(chariot_t *chariot){
-  servo_chariot.attach(SERVO_PIN);
-  pinMode(MOTOR_PIN1, OUTPUT);
-  pinMode(MOTOR_PIN2, OUTPUT);
-  pinMode(ENABLE_PIN, OUTPUT);
-  pinMode(CAPTEUR1, INPUT_PULLUP);
-  pinMode(CAPTEUR2, INPUT_PULLUP);
-  pinMode(CAPTEUR3, INPUT_PULLUP);
-  pinMode(CAPTEUR4, INPUT_PULLUP);
-}
-
-void move_right(){
-  int motorValue;
-  sens=vers_droite;
-  digitalWrite(MOTOR_PIN2, LOW);
-  for(int motorValue = 0 ; motorValue <= 250; motorValue +=10){
-    digitalWrite(MOTOR_PIN1, motorValue); 
-    delay(30);
-   ]
-}
-
-void move_left(){
-  int motorValue;
-  sens=vers_gauche;
-  digitalWrite(MOTOR_PIN1, LOW);
-  for(int motorValue = 0 ; motorValue <= 250; motorValue +=RAMP_INCREASE){
-    digitalWrite(MOTOR_PIN2, motorValue); 
-    delay(RAMP_DELAY);
+  servo_chariot.attach(PIN_SERVO);
+  pinMode(PIN_1_MOTOR, OUTPUT);
+  pinMode(PIN_2_MOTOR, OUTPUT);
+  for(int i = 0 ; i < SENSOR_POSITION_SIZE; i++)
+  {
+    pinMode(chariot->sensor_position_array[i],INPUT_PULLUP);
   }
 }
 
-void hold(){ 
-  int motorValue;
-  if (sens==vers_droite){
-    digitalWrite(MOTOR_PIN2, LOW);
-    for(int motorValue = 250 ; motorValue >= 0; motorValue -=RAMP_INCREASE){
-      digitalWrite(MOTOR_PIN1, motorValue); 
-      delay(RAMP_DELAY);
+void charriot_right(chariot_t *chariot){
+  chariot->speed += MOTOR_SLICE_VALUE;
+  delay(SLICE_DELAY_MS);
+}
+
+void charriot_left(chariot_t *chariot){
+  chariot->speed -= MOTOR_SLICE_VALUE;
+  delay(SLICE_DELAY_MS);
+}
+
+void choose_way(chariot_t *chariot){
+  if(chariot->speed> -MOTOR_SLICE_VALUE && chariot->speed <MOTOR_SLICE_VALUE)
+  {
+    chariot->way = WAY_STOP;
+    chariot->speed = 0;
+    analogWrite(PIN_1_MOTOR, LOW);
+    analogWrite(PIN_2_MOTOR, LOW);
+
+  }else if (chariot -> speed >0)
+  {
+    chariot->speed = chariot->speed >255 ? 255:chariot->speed;
+    chariot->way = WAY_RIGHT;
+    analogWrite(PIN_2_MOTOR, LOW);
+    analogWrite(PIN_1_MOTOR,chariot->speed); 
+  }else if (chariot->speed < 0)
+  {
+    chariot->speed = chariot->speed < -255 ? -255:chariot->speed;
+    chariot->way = WAY_LEFT;
+
+    analogWrite(PIN_1_MOTOR, LOW);
+    analogWrite(PIN_2_MOTOR,-(chariot->speed)); 
+
+  }
+}
+
+void charriot_stop(chariot_t *chariot)
+{
+  while (chariot->way !=WAY_STOP)
+  {
+    if(chariot->way == WAY_RIGHT)
+      chariot->left(chariot);
+    else
+      chariot->right(chariot);
+    chariot->move(chariot);
+  }
+  
+}
+
+void move_position(chariot_t *chariot,char position){
+
+  chariot->stop(chariot);
+  for(int i = 0  ; i < MOTOR_MOVE_POSITION_MULTIPLE ; i++)
+  {
+    if(chariot->position > position)
+    {
+      chariot->right(chariot);
+    }else if (chariot->position < position)
+    {
+      chariot->left(chariot);
     }
-  } else if (sens==vers_gauche){
-    digitalWrite(MOTOR_PIN1, LOW);
-    for(int motorValue = 250 ; motorValue >= 0; motorValue -=RAMP_INCREASE){
-      digitalWrite(MOTOR_PIN2, motorValue); 
-      delay(RAMP_DELAY);
+    else
+    {
+      break;
     }
+    chariot->move(chariot);
+    chariot->check(chariot);
   }
-  // pas nécessaire mais par sécurité je l'ai mis
-  else if (sens==arret){
-    digitalWrite(MOTOR_PIN1, LOW);
-    digitalWrite(MOTOR_PIN2, LOW);
+  while (chariot->position != position)
+  { 
+    chariot->move(chariot);
+    chariot->check(chariot);
+    delay(DELAY_CHECK_MOVE_POSITION);
   }
+  chariot->stop(chariot);
+  
 }
-
-
-void move_position(chariot_t *chariot,uint8_t position){
-  if (chariot.position > position){
-    move_right();
-  } else if(chariot.position < position){
-    move_left(); 
-  } else {
-    hold();
-  }
-}
-
-
 
 void check(chariot_t *chariot){
   
-  if (digitalread(CAPTEUR1) == 1){
-    position = 1;
-  }
-  if (digitalread(CAPTEUR2) == 1){
-    position = 2;
-  }
-  if (digitalread(CAPTEUR3) == 1){
-    position = 3;
-  }
-  if (digitalread(CAPTEUR4) == 1){
-    position = 4;
+  for(int i = 0 ; i < SENSOR_POSITION_SIZE ; i ++)
+  {
+    if (digitalRead(chariot->sensor_position_array[i]))
+    {
+      chariot->position = i;
+      return;
+    }
   }
 }
 
 // Pour vider dans le verre
 void pour(chariot_t *chariot){
-        servo_chariot.write(ANGLE_MAX);  
-        delay(POUR_TIME); // temps chariot abaissé
-        servo_chariot.write(ANGLE_MIN); 
+        for(int i = SERVO_ANGLE_MIN; i < SERVO_ANGLE_MAX ; i++)
+        {
+          servo_chariot.write(i);  
+          delay(SERVO_DELAY_MS); // temps chariot abaissé
+        }
+        delay(POUR_TIME_MS); // temps chariot abaissé
+        for(int i = SERVO_ANGLE_MAX ; i >= SERVO_ANGLE_MIN ; i--)
+        {
+          servo_chariot.write(i);  
+          delay(SERVO_DELAY_MS); // temps chariot abaissé
+        } 
 }
 
 
 
 static chariot_t chariot={
   .position = 0,
-  .init=init,
-  .move_right = move_right,
-  .move_left = move_left,
+  .way= WAY_STOP,
+  .speed = 0,
+  .sensor_position_array = {
+    PIN_SENSOR_POSITION_0,
+    PIN_SENSOR_POSITION_1,
+    PIN_SENSOR_POSITION_2,
+    PIN_SENSOR_POSITION_3
+  },
+  .init=init_chariot,
+  .right = charriot_right,
+  .left = charriot_left,
+  .stop = charriot_stop,
   .move_position=move_position,
+  .move=choose_way,
   .check = check,
   .pour=pour
   };
--- main/chariot.hpp
+++ main/chariot.hpp
@@ -1,52 +1,33 @@
 
-#include <stdint.h>
+#include "config.h"
 
 #ifndef CHARIOT_H
 #define CHARIOT_H
 
-#define POSITION_CENTER 0
-
-#define SERVO_PIN 7
-//Ports de commande du moteur B
-#define MOTOR_PIN1 8
-#define MOTOR_PIN2 9
-#define ENABLE_PIN 6
-
-#define BOUTON_DROIT_PIN 12
-#define BOUTON_GAUCHE_PIN 13
-#define CAPTEUR1 2
-#define CAPTEUR2 3
-#define CAPTEUR3 4
-#define CAPTEUR4 5
-
-// Pour vider dans le verre
-#define ANGLE_MIN 0
-#define ANGLE_MAX 180
-#define POUR_TIME 5000
-
-// rampes de commandes pour accélération du moteur, 30ms entre chaque augmentation, 25 paliers de 10 = 250
-#define RAMP_INCREASE 10
-#define RAMP_DELAY 30
 
 
-enum sens {
-  vers_droite;
-  vers_gauche;
-  arret;
-};
-
+typedef enum way 
+{
+  WAY_RIGHT,
+  WAY_LEFT,
+  WAY_STOP
+} way_t;
 
 
 typedef struct chariot
 {
-  uint8_t position;  
+  char position;  
+  way_t way; 
+  int speed;
+  char sensor_position_array[SENSOR_POSITION_SIZE];
   void (*init)(struct chariot *chariot);
-  void (*move_right)();
-  void (*move_left)();
-  void (*move_position)(struct chariot *chariot,uint8_t position);
+  void (*right)(struct chariot *chariot);
+  void (*left)(struct chariot *chariot);
+  void (*stop)(struct chariot *chariot);
+  void (*move_position)(struct chariot *chariot,char position);
+  void (*move)(struct chariot *chariot);
   void (*check)(struct chariot *chariot);
   void (*pour)(struct chariot *chariot);
-  void (*hold)();
 }chariot_t;
 
 #endif
